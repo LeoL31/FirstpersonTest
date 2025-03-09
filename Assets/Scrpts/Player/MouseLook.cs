7333b525c5488d473e2b7e87e86be1e547ef8d82
Léandre Lack <fsteve31@outlook.com>
2025-03-09 23:14:38 +0100

    PlayerMovment Update
    With Switch case

--- Assets/Scrpts/Player/MouseLook.cs
+++ Assets/Scrpts/Player/MouseLook.cs
@@ -2,34 +2,36 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-    [SerializeField] float MouseSensitivity = 100f;
-    [SerializeField] float MaxLookUp = 90f;
-    [SerializeField] float MaxLookDown = -90f;
+    [Header("Mouse")]
+    [SerializeField] float MouseXSensitivity = 100f;
+    [SerializeField] float MouseYSensitivity = 100f;
+    [SerializeField] float MaxLookDown = 90f;
+    [SerializeField] float MaxLookUp = -90f;
+    [Header("Setup")]
     [SerializeField] bool LockMouse = true;
     [SerializeField] Transform playerBody;
-    
-
 
     private float xRotation = 0f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         if (LockMouse == true) 
         {
             UnityEngine.Cursor.lockState = CursorLockMode.Locked;
+            UnityEngine.Cursor.visible = false;
         }
         else
         {
             UnityEngine.Cursor.lockState = CursorLockMode.None;
+            UnityEngine.Cursor.visible = true;
         }   
     }
 
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X") * MouseXSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * MouseYSensitivity * Time.deltaTime;
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, MaxLookDown, MaxLookUp);
--- Assets/Scrpts/Player/PlayerMovment.cs
+++ Assets/Scrpts/Player/PlayerMovment.cs
@@ -1,25 +1,41 @@
+using UnityEditor.ShaderGraph;
 using UnityEngine;
 
+//Forces the object to have a CharacterController component
 [RequireComponent(typeof(CharacterController))]
 
 public class PlayerMovment : MonoBehaviour
 {
-    [SerializeField] CharacterController controller;
-    [SerializeField] float playerDefaultSpeed = 8f;
-    [SerializeField] float playerSneakingSpeed =4;
-    [SerializeField] float playerRunningSpeed = 11f;
-    [SerializeField] float playerMovmenAcceleration = 8f;
-    [SerializeField] float jumpHeight = 3.0f;
-    [SerializeField] float gravity = -9.81f;
+    enum MovementState { Walking, Running, Crouching }
 
-    [SerializeField] Transform groundCheck;
-    [SerializeField] float groundDistance = 0.4f;
-    [SerializeField] LayerMask groundMask;
+    [Header("Movement Speeds")]
+    [SerializeField] private float walkSpeed = 8f;
+    [SerializeField] private float runSpeed = 11f;
+    [SerializeField] private float crouchSpeed = 4f;
+    [SerializeField] private float acceleration = 8f;
 
-    Vector3 velocity;
+    [Header("Jumping & Gravity")]
+    [SerializeField] private float jumpHeight = 3.0f;
+    [SerializeField] private float gravity = -9.81f;
+
+    [Header("Setup")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundDistance = 0.4f;
+    [SerializeField] private LayerMask groundMask;
+
+    private CharacterController controller;
+    private Vector3 velocity;
     private bool isGrounded;
     private float currentSpeed;
-    private float targetSpeed;
+    private MovementState currentState;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        currentState = MovementState.Walking; 
+        currentSpeed = walkSpeed;
+    }
+
 
     void Update()
     {
@@ -30,27 +46,35 @@ public class PlayerMovment : MonoBehaviour
             velocity.y = -2;
         }
 
-        // Get input for movement
+        // Handle Movement State Switching
+        if (Input.GetKey(KeyCode.LeftShift)) currentState = MovementState.Running;
+        else if (Input.GetKey(KeyCode.LeftControl)) currentState = MovementState.Crouching;
+        else currentState = MovementState.Walking;
+
+        // Set speed based on state
+        switch (currentState)
+        {
+            case MovementState.Walking:
+                currentSpeed = walkSpeed;
+                break;
+            case MovementState.Running:
+                currentSpeed = runSpeed;
+                break;
+            case MovementState.Crouching:
+                currentSpeed = crouchSpeed;
+                break;
+        }
+
+
+        // Move player
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-
-        // Walking & Running & Sneaking
         Vector3 move = transform.right * x + transform.forward * z;
-        targetSpeed = playerDefaultSpeed;
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            targetSpeed = playerRunningSpeed; // Running
-        }
-        else if (Input.GetKey(KeyCode.LeftControl))
-        {
-            targetSpeed = playerSneakingSpeed; // Sneaking
-        }
-        else
-        {
-            targetSpeed = playerDefaultSpeed; // Walking
-        }
-        currentSpeed = Mathf.Lerp(currentSpeed, targetSpeed, playerMovmenAcceleration * Time.deltaTime);
+
+        currentSpeed = Mathf.Lerp(currentSpeed, currentSpeed, acceleration * Time.deltaTime);
         controller.Move(move * currentSpeed * Time.deltaTime);
+
+        // Debug.Log("Current Speed: " + currentSpeed);
 
 
         // Jumping
@@ -60,5 +84,8 @@ public class PlayerMovment : MonoBehaviour
         }
         velocity.y += Physics.gravity.y * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
+
+
+
     }
 }
