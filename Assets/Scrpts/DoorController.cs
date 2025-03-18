eeebc3623fb7472d619fd64f5e058504e34a4b98
Léandre Lack <fsteve31@outlook.com>
2025-03-18 19:44:57 +0100

    Door Spamming Fix

--- Assets/Scrpts/DoorController.cs
+++ Assets/Scrpts/DoorController.cs
@@ -2,28 +2,61 @@ using UnityEngine;
 
 public class DoorController : MonoBehaviour, IInteractable
 {
+
+    [SerializeField] bool randomDoorOnStart = true;
+    [SerializeField] bool IsLocked = false;
+
     private Animator doorAnimation;
     private bool isOpen;
+    private bool isMoving = true; // checking if the Animations is Playing so you cant spamm the door open and close
 
     private void Awake()
     {
         doorAnimation = GetComponent<Animator>();
     }
-
-    public void Interact() 
+    private void Start()
     {
-     if (!isOpen)
+        if (randomDoorOnStart == true)
         {
-            doorAnimation.Play("DoorOpen", 0, 0.0f);
-            isOpen = true;
-        }
-
-        else
-        {
-            doorAnimation.Play("DoorClose", 0, 0.0f);
-            isOpen = false;
+            isOpen = Random.Range(0, 2) == 0 ? false : true;
+            if (isOpen)
+            {
+                doorAnimation.Play("DoorOpen", 0, 0.0f);
+            }
+            else
+            {
+                doorAnimation.Play("DoorClose", 0, 0.0f);
+            }
         }
     }
 
+
+    public void Interact() 
+    {
+     if (isOpen == true && isMoving == false && IsLocked == false)
+        {
+            isMoving = true;
+            doorAnimation.Play("DoorClose", 0, 0.0f);
+        }
+        else if (isOpen == false && isMoving == false && IsLocked == false)
+        {
+            isMoving = true;
+            doorAnimation.Play("DoorOpen", 0, 0.0f);
+        }
+    }
+
+    
+
+    // these Functions are called by the Animation Event
+    public void DoorOpenAniDone() 
+    {
+        isOpen = true;
+        isMoving = false;
+    }
+    public void DoorCloseAniDone()
+    {
+        isOpen = false;
+        isMoving = false;
+    }
 }
 
